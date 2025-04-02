7d9d97aa2c081f76cd6a316b3d80532da25836d3
Daniel Lima <dan.jhony14@gmail.com>
2025-04-01 21:29:50 -0300

    Estilização

---
+++ carrinho_compras/src/components/modules/shared/SlidePanel.vue
@@ -0,0 +1,65 @@
+<script setup lang="ts">
+const props = defineProps({
+  title:{
+    type: String,
+    required: true
+  },
+  visible:{
+    type: Boolean,
+    required: true
+  }
+
+})
+const closePanel = () => {
+  emit('update:visible', false)
+
+}
+const emit = defineEmits(['update:visible'])
+</script>
+
+<template>
+  <div class="panel-wrap" :class="{visible: visible}">
+  <div class="panel">
+    <div class="">
+      <button class="btn error--outline" @click="closePanel">Fechar</button>
+      <h2>{{ props.title }}</h2>
+    </div>
+  </div>
+</div>
+
+  <slot name="content"></slot>
+
+</template>
+
+<style lang="scss" scoped>
+.panel {
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  width: 100em;
+  background-color: #333;
+  color: #eee;
+  overflow: auto;
+  padding: 1em;
+
+  &-wrap{
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    left: 70em;
+    right: 0;
+    width: 30em;
+    transform: translateX(100%);
+    transition: 1s ease-out;
+    &.visible {
+      transform: translateX(0%);
+    }
+  }
+
+
+
+}
+
+</style>
---
+++ carrinho_compras/src/views/HomePage.vue
@@ -0,0 +1,85 @@
+<script setup lang="ts">
+import SlidePanel from '@/components/modules/shared/SlidePanel.vue';
+import { ref } from 'vue';
+
+const cartVisible = ref(false);
+
+const changeCartVisibility = () => {
+  cartVisible.value = !cartVisible.value;
+  console.log(cartVisible.value)
+}
+
+
+</script>
+
+<template>
+  <main>
+    <h1>Shopping</h1>
+
+    <button class="btn sucess"> Adicionar ao Carrinho</button>
+    <button class="btn error ml-1"> Limpar Carrinho</button>
+    <button class="btn error ml-1" @click="changeCartVisibility">
+      Abrir carrinho
+    </button>
+    <SlidePanel
+    title="Cart"
+    :visible="cartVisible"
+    @update:visible="changeCartVisibility"
+    v-if="cartVisible"
+    >
+
+    </SlidePanel>
+  </main>
+</template>
+
+<style lang="scss" scoped>
+@import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
+.btn {
+  padding: 10px;
+  border-radius: 5px;
+
+  font-family: "Poppins", sans-serif;
+  font-weight: 700;
+  font-style: normal;
+
+  &.primary{
+    background-color: #98fff6;
+    color: #fff;
+    &:hover{
+      background-color: #fff;
+      color: #000;
+    }
+  }
+  &.sucess{
+    background-color: #77ff77;
+    color: #000;
+    &:hover{
+      background-color: #8ef98e;
+      color: #000;
+    }
+  }
+  &.error {
+    background-color: #ff7777;
+    color: #fff;
+    &:hover{
+      background-color: #fa9b9b;
+      color: #000;
+    }
+    &--outline{
+      background-color: transparent;
+      color: #ff7777;
+
+      &:hover {
+        background-color: #ff7777;
+        border: 1px solid #ff7777 ;
+        color: #fff;
+      }
+    }
+  }
+}
+
+.ml-1 {
+  margin-left: 1rem;
+}
+</style>
+
